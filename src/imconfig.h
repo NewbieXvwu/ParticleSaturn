fe452e27c437471f3a8cf83691d3f4e27b1fbb25
NewbieXvwu <NewbieXvwu@qq.com>
2025-12-08 20:46:31 +0800

    refactor: convert imgui to git submodule
    - Replace vendored imgui with submodule pointing to official repo
    - Move custom imconfig.h to src/ with IMGUI_USER_CONFIG
    - Add IMGUI_IMPL_OPENGL_LOADER_GLAD for glad compatibility
    - Update vcxproj include paths for backends folder
    - Enable vcpkg manifest mode

--- src/imconfig.h
+++ src/imconfig.h
@@ -37,6 +37,9 @@
 #define IMGUI_DISABLE_DEMO_WINDOWS                        // Disable demo windows: ShowDemoWindow()/ShowStyleEditor() will be empty.
 #define IMGUI_DISABLE_DEBUG_TOOLS                         // Disable metrics/debugger and other debug tools: ShowMetricsWindow(), ShowDebugLogWindow() and ShowIDStackToolWindow() will be empty.
 
+//---- Use glad as OpenGL loader
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+
 //---- Don't implement some functions to reduce linkage requirements.
 //#define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS   // [Win32] Don't implement default clipboard handler. Won't use and link with OpenClipboard/GetClipboardData/CloseClipboard etc. (user32.lib/.a, kernel32.lib/.a)
 //#define IMGUI_ENABLE_WIN32_DEFAULT_IME_FUNCTIONS          // [Win32] [Default with Visual Studio] Implement default IME handler (require imm32.lib/.a, auto-link for Visual Studio, -limm32 on command-line for MinGW)
