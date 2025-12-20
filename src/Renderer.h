1dc5d4551929c8fa681ddf075140ca1470898a7c
NewbieXvwu <NewbieXvwu@qq.com>
2025-12-20 12:32:10 +0800

    refactor: 将 Renderer 大型函数实现移到 cpp 文件，减少编译时间

--- src/Renderer.h
+++ src/Renderer.h
@@ -50,21 +50,13 @@ struct UniformCache {
 
 namespace Renderer {
 
-// 创建着色器程序
+// 声明 (实现在 Renderer.cpp)
+unsigned int CreateProgramImpl(const char* vertexSrc, const char* fragmentSrc);
+unsigned int GenerateFBMTextureImpl(int width, int height);
+
+// 创建着色器程序 (转发到实现)
 inline unsigned int CreateProgram(const char* vertexSrc, const char* fragmentSrc) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs      = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertexSrc, 0);
-    glCompileShader(vs);
-    unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragmentSrc, 0);
-    glCompileShader(fs);
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-    return program;
+    return CreateProgramImpl(vertexSrc, fragmentSrc);
 }
 
 // 初始化 Uniform 缓存
@@ -358,63 +350,9 @@ inline unsigned int GenerateNoiseTexture(int width = 256, int height = 256) {
     return tex;
 }
 
-// 生成 FBM 噪声纹理 (用于行星表面)
+// 生成 FBM 噪声纹理 (用于行星表面) - 转发到实现
 inline unsigned int GenerateFBMTexture(int width = 512, int height = 512) {
-    // 辅助函数: 2D 哈希噪声
-    auto hash = [](float x, float y) -> float {
-        return fmodf(sinf(x * 12.9898f + y * 78.233f) * 43758.5453f, 1.0f);
-    };
-
-    // 辅助函数: 平滑插值噪声
-    auto noise = [&](float x, float y) -> float {
-        int   ix = (int)floorf(x);
-        int   iy = (int)floorf(y);
-        float fx = x - ix;
-        float fy = y - iy;
-        float ux = fx * fx * (3.0f - 2.0f * fx);
-        float uy = fy * fy * (3.0f - 2.0f * fy);
-
-        float a = hash((float)ix, (float)iy);
-        float b = hash((float)(ix + 1), (float)iy);
-        float c = hash((float)ix, (float)(iy + 1));
-        float d = hash((float)(ix + 1), (float)(iy + 1));
-
-        return a + (b - a) * ux + (c - a) * uy + (a - b - c + d) * ux * uy;
-    };
-
-    // FBM: 5 层噪声叠加
-    auto fbm = [&](float x, float y) -> float {
-        float value = 0.0f;
-        float amp   = 0.5f;
-        for (int i = 0; i < 5; i++) {
-            value += amp * noise(x, y);
-            x *= 2.0f;
-            y *= 2.0f;
-            amp *= 0.5f;
-        }
-        return value;
-    };
-
-    std::vector<unsigned char> data(width * height);
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            float u     = (float)x / width * 16.0f;
-            float v     = (float)y / height * 16.0f;
-            float value = fbm(u, v);
-            data[y * width + x] = (unsigned char)(value * 255.0f);
-        }
-    }
-
-    unsigned int tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, height, 0, GL_RED, GL_UNSIGNED_BYTE, data.data());
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    return tex;
+    return GenerateFBMTextureImpl(width, height);
 }
 
 } // namespace Renderer
